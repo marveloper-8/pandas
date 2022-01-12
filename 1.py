ed27932bdd93d54297d0b0eacce98cf95b7e6050
Joshua Equere <marveloper.8@gmail.com>
2022-01-12 18:43:27 +0100

    pandas

---
+++ 1.py
@@ -0,0 +1,46 @@
+# import pandas as pd
+
+# a = {
+#     'cars': ['BMW', 'Volvo', 'Ford'],
+#     'passings': [3, 7, 2]
+# }
+# b = pd.DataFrame(a)
+# print(b)
+
+# print(pd.__version__)
+
+# import pandas as pd
+# a = [1, 7, 2]
+# b = pd.Series(a)
+# print(b)
+# print(b[0])
+
+# c = pd.Series(a, index=["x", "y", "z"])
+# print(c)
+# print(c["y"])
+
+# d = {
+#     "day1": 420,
+#     "day2": 380,
+#     "day3": 390
+# }
+# e = pd.Series(d)
+# print(e)
+# f = pd.Series(d, index=["day1", "day2"])
+# print(f)
+
+# g = {
+#     "h": [420, 380, 390],
+#     "i": [50, 40, 45]
+# }
+# j = pd.DataFrame(g)
+# print(j)
+
+import pandas as pd
+
+a = {
+    "b": [420, 380, 390],
+    "c": [50, 40, 45]
+}
+d = pd.DataFrame(a)
+print(d)
